5ef365fe58e62c9ad00ae6ba949eda5e3f849b18
nesquik <99757380+deliciousNesquik@users.noreply.github.com>
2023-04-07 21:09:24 +0500

    Delete scripts directory

--- scripts/date.sh
+++
@@ -1,7 +0,0 @@
-echo "установлен часовой пояс Москва"
-sudo unlink /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-date
-
-echo "установка модулей для python"
-pip install -r requirements.txt
